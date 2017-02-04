7904f7b0c77b983f0a716e314ea424a4672977dc
QP <qp@powerme.com.sg>
2017-02-05 04:35:53 +0800

    added concatenation into make protask

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+// Include gulp
 var gulp = require('gulp');
+
+// Include plugins
 var concat = require('gulp-concat');
+var ugligy = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
     console.log('***********************');
@@ -8,5 +13,7 @@ gulp.task('default', function() {
 gulp.task('make', function(){
     return gulp.src('src/core/*.js')
         .pipe(concat('graphy.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(ugligy())
         .pipe(gulp.dest('build/v1.0.0'))
 });
