904c047a8c2dfc4e3f60675c9365afecebf7a380
QP <qp@powerme.com.sg>
2017-02-05 04:06:58 +0800

    restructured folders. added make task for concatenating

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-    console.log('TEST GULP');
+    console.log('***********************');
+});
+
+gulp.task('make', function(){
+    return gulp.src('src/core/*.js')
+        .pipe(concat('graphy.js'))
+        .pipe(gulp.dest('build/v1.0.0'))
 });
